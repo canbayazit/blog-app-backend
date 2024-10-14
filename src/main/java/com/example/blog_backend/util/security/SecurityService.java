3d9608ea80196317cd573dbbaa2294ce721c5675
Can Bayazit <rovahelsing@gmail.com>
2024-10-14 22:18:40 +0300

    updated security service as dynamic role-based

--- src/main/java/com/example/blog_backend/util/security/SecurityService.java
+++ src/main/java/com/example/blog_backend/util/security/SecurityService.java
@@ -2,6 +2,7 @@ package com.example.blog_backend.util.security;
 
 import com.example.blog_backend.entity.UserEntity;
 import com.example.blog_backend.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,10 +12,11 @@ import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class SecurityService implements UserDetailsService {
-
     private final UserRepository userRepository;
 
     public SecurityService(UserRepository userRepository) {
@@ -22,21 +24,16 @@ public class SecurityService implements UserDetailsService {
     }
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        Optional<UserEntity> user = userRepository.findByEmail(email);
+        Optional<UserEntity> user = userRepository.findByEmailWithRoles(email);
 
         if (user.isEmpty()) {
             throw new UsernameNotFoundException("Could not find User with email =" + email);
         }
 
-        /*Set<GrantedAuthority> authorities = user.get().getRoles().stream()
+        Set<GrantedAuthority> authorities = user.get().getRoles().stream()
                 .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName()))
                 .collect(Collectors.toSet());
 
-        return new User(email, user.get().getPassword(), authorities);*/
-
-        return new User(email,
-                user.get().getPassword(),
-                Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
-
+        return new User(email, user.get().getPassword(), authorities);
     }
 }
