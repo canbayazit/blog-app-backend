cc9b8e8d9f3650b09434b4f8aa23133c8fd5072e
Can Bayazit <rovahelsing@gmail.com>
2024-10-19 01:23:27 +0300

    added custom query to retrieve the roles of the users since the role in the user entity was defined as lazy

--- src/main/java/com/example/blog_backend/repository/UserRepository.java
+++ src/main/java/com/example/blog_backend/repository/UserRepository.java
@@ -11,6 +11,14 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends BaseRepository<UserEntity> {
     Optional<UserEntity> findByEmail(String email);
+
+    // Security Service de findByEmail kullanınca user entity de roller lazy olduğu için roller gelmiş olmuyor
+    // ve bu yüzden user.get().getRoles() dediğimizde veri gelmiyor 401 alıyoruz.
+    // Bunun sebebi Security Service de UserRepository den findByEmail ile bir sorgu çalıştırdığımızda
+    // Hibernate bir session açar ve veritabanından ilgili kullanıcıyı yükler.
+    // Ancak bu session, metot tamamlanıp işlem bittiğinde kapanır. O yüzden user.get().getRoles() dediğimizde rolleri
+    // lazy olarak yükleyemeyiz. Çünkü Lazy olan veriyi session açıkken çekebiliriz. Bu yüzden aşağıdaki gibi
+    // custom query tanımladık. Fetch ile lazy olan role verisini yüklemesini söylüyoruz
     @Query("SELECT u FROM UserEntity u JOIN FETCH u.roles WHERE u.email = :email")
     Optional<UserEntity> findByEmailWithRoles(@Param("email") String email);
 }
