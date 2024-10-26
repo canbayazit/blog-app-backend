b389b7bff5e3e18d507b7cd9539b171d9432c1cf
Can Bayazit <rovahelsing@gmail.com>
2024-10-26 05:49:15 +0300

    added user controller and service

--- src/main/java/com/example/blog_backend/controller/UserController.java
+++ src/main/java/com/example/blog_backend/controller/UserController.java
@@ -1,28 +1,88 @@
 package com.example.blog_backend.controller;
 
-import com.example.blog_backend.core.controller.BaseController;
-import com.example.blog_backend.entity.UserEntity;
-import com.example.blog_backend.mapper.UserMapper;
+import com.example.blog_backend.model.requestDTO.UserEmailRequestDTO;
+import com.example.blog_backend.model.requestDTO.UserPasswordUpdateRequestDTO;
 import com.example.blog_backend.model.requestDTO.UserProfileRequestDTO;
+import com.example.blog_backend.model.responseDTO.UserProfileResponseDTO;
 import com.example.blog_backend.model.responseDTO.UserResponseDTO;
-import com.example.blog_backend.repository.UserRepository;
 import com.example.blog_backend.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 
 @RestController
 @RequestMapping("user")
-public class UserController extends BaseController<
-        UserEntity,
-        UserResponseDTO,
-        UserProfileRequestDTO,
-        UserMapper,
-        UserRepository,
-        UserService> {
+public class UserController {
     private final UserService userService;
     public UserController(UserService userService) {
-        super(userService);
         this.userService = userService;
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("get-all")
+    public ResponseEntity<List<UserResponseDTO>> getAll() {
+        return new ResponseEntity<>(userService.getAll(), HttpStatus.OK);
+    }
+
+    @PreAuthorize("hasRole('USER') and @userService.isSelf(#uuid)")
+    @PutMapping("/update/profile/{uuid}")
+    public ResponseEntity<UserProfileResponseDTO> updateProfile(@PathVariable UUID uuid, @Valid @RequestBody UserProfileRequestDTO requestDTO) {
+        UserProfileResponseDTO updatedProfile = userService.updateProfile(uuid, requestDTO);
+        if (updatedProfile != null) {
+            return new ResponseEntity<>(updatedProfile, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PreAuthorize("hasRole('USER') and @userService.isSelf(#id)")
+    @PutMapping("/update/email/{id}")
+    public ResponseEntity<Boolean> updateEmail(@PathVariable UUID id, @Valid @RequestBody UserEmailRequestDTO updateEmailDTO) {
+        Boolean isUpdated = userService.updateEmail(id, updateEmailDTO);
+        if (isUpdated != null && isUpdated) {
+            return new ResponseEntity<>(true, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PreAuthorize("hasRole('USER') and @userService.isSelf(#uuid)")
+    @PutMapping("/update/password/{uuid}")
+    public ResponseEntity<Boolean> updatePassword(@PathVariable UUID uuid, @Valid @RequestBody UserPasswordUpdateRequestDTO passwordUpdateRequestDTO) {
+        Boolean isUpdated = userService.updatePassword(uuid, passwordUpdateRequestDTO);
+        if (isUpdated != null && isUpdated) {
+            return new ResponseEntity<>(true, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PreAuthorize("hasRole('ADMIN') or @userService.isSelf(#uuid)")
+    @GetMapping("/get/{uuid}")
+    public ResponseEntity<UserResponseDTO> getByUUID(@PathVariable UUID uuid) {
+        UserResponseDTO dto = userService.getByUUID(uuid);
+        if (dto != null) {
+            return new ResponseEntity<>(dto, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PreAuthorize("hasRole('ADMIN') or @userService.isSelf(#uuid)")
+    @DeleteMapping("/delete/{uuid}")
+    public ResponseEntity<Boolean> deleteByUUID(@PathVariable UUID uuid) {
+        Boolean isDeleted = userService.deleteByUUID(uuid);
+        if (isDeleted) {
+            return new ResponseEntity<>(true, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(false, HttpStatus.NOT_FOUND);
+        }
     }
 
 
--- src/main/java/com/example/blog_backend/service/UserService.java
+++ src/main/java/com/example/blog_backend/service/UserService.java
@@ -1,33 +1,106 @@
 package com.example.blog_backend.service;
 
-import com.example.blog_backend.core.service.BaseService;
 import com.example.blog_backend.entity.UserEntity;
 import com.example.blog_backend.mapper.UserMapper;
+import com.example.blog_backend.model.requestDTO.UserEmailRequestDTO;
+import com.example.blog_backend.model.requestDTO.UserPasswordUpdateRequestDTO;
 import com.example.blog_backend.model.requestDTO.UserProfileRequestDTO;
+import com.example.blog_backend.model.responseDTO.UserProfileResponseDTO;
 import com.example.blog_backend.model.responseDTO.UserResponseDTO;
-import com.example.blog_backend.repository.RoleRepository;
 import com.example.blog_backend.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 
 @Service
-public class UserService extends BaseService<
-        UserEntity,
-        UserResponseDTO,
-        UserProfileRequestDTO,
-        UserMapper,
-        UserRepository> {
+public class UserService {
 
     private final UserRepository userRepository;
-    private final RoleRepository roleRepository;
     private final UserMapper userMapper;
 
-    public UserService(UserMapper getMapper, UserRepository userRepository,
-                       RoleRepository roleRepository, UserMapper userMapper) {
-        super(getMapper, userRepository);
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, UserMapper userMapper, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
-        this.roleRepository = roleRepository;
         this.userMapper = userMapper;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public List<UserResponseDTO> getAll() {
+        return userMapper.entityListToDTOList(userRepository.findAll());
+    }
+
+    @Transactional
+    public UserProfileResponseDTO updateProfile(UUID uuid, UserProfileRequestDTO requestDTO) {
+        UserEntity entity = userRepository.findByUuid(uuid).orElse(null);
+        if (entity != null) {
+            entity = userMapper.requestDtoToExistEntity(entity, requestDTO);
+            userRepository.save(entity);
+            return userMapper.entityToUpdateResponseDTO(entity);
+        } else {
+            return null;
+        }
+    }
+
+    @Transactional
+    public Boolean updateEmail(UUID id, UserEmailRequestDTO userEmailRequestDTO) {
+        UserEntity entity = userRepository.findByUuid(id).orElse(null);
+        if (entity != null) {
+            entity = userMapper.requestDtoToExistEntity(entity, userEmailRequestDTO);
+            userRepository.save(entity);
+            return true;
+        } else {
+            return null;
+        }
+    }
+
+    @Transactional
+    public Boolean updatePassword(UUID id, UserPasswordUpdateRequestDTO passwordUpdateRequestDTO) {
+        UserEntity entity = userRepository.findByUuid(id).orElse(null);
+        if (entity != null) {
+            // Mevcut şifreyi doğrulama
+            if (!passwordEncoder.matches(passwordUpdateRequestDTO.getCurrentPassword(), entity.getPassword())) {
+                throw new IllegalArgumentException("Mevcut şifre hatalı");
+            } else {
+                System.out.printf(passwordEncoder.encode(passwordUpdateRequestDTO.getNewPassword()));
+                entity.setPassword(passwordEncoder.encode(passwordUpdateRequestDTO.getNewPassword()));
+                userRepository.save(entity);
+                return true;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    public UserResponseDTO getByUUID(UUID uuid) {
+        UserEntity entity = userRepository.findByUuid(uuid).orElse(null);
+        if (entity != null) {
+            return userMapper.entityToDTO(entity);
+        } else {
+            return null;
+        }
+    }
+
+    @Transactional
+    public Boolean deleteByUUID(UUID uuid) {
+        UserEntity entity = userRepository.findByUuid(uuid).orElse(null);
+        if (entity != null) {
+            userRepository.delete(entity);
+            return Boolean.TRUE;
+        } else {
+            return Boolean.FALSE;
+        }
+    }
+
+    public boolean isSelf(UUID id) {
+        String currentUserEmail = SecurityContextHolder.getContext().getAuthentication().getName();
+        UserEntity currentUser = userRepository.findByEmail(currentUserEmail).orElse(null);
+        return currentUser != null && currentUser.getUuid().equals(id);
     }
 
 }
