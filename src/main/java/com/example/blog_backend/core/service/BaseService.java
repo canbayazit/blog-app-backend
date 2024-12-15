2ad0edc018697c5bd3e863e025e0f7ae97017b17
Can Bayazit <rovahelsing@gmail.com>
2024-12-15 22:26:57 +0300

    updated base service structure

--- src/main/java/com/example/blog_backend/core/service/BaseService.java
+++ src/main/java/com/example/blog_backend/core/service/BaseService.java
@@ -1,68 +1,22 @@
 package com.example.blog_backend.core.service;
 
 import com.example.blog_backend.core.dto.BaseDTO;
-import com.example.blog_backend.core.entity.BaseEntity;
-import com.example.blog_backend.core.mapper.IBaseMapper;
-import com.example.blog_backend.core.repository.BaseRepository;
-import lombok.AllArgsConstructor;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
 
-@AllArgsConstructor
-public abstract class BaseService<
-        Entity extends BaseEntity,
+public interface BaseService<
         DTO extends BaseDTO,
-        RequestDTO,
-        Mapper extends IBaseMapper<DTO, Entity, RequestDTO>,
-        Repository extends BaseRepository<Entity>
-        > {
+        RequestDTO> {
 
-    private final Mapper getMapper;
+    DTO save(RequestDTO requestDTO);
 
-    private final Repository getRepository;
+    List<DTO> getAll();
 
-    @Transactional
-    public DTO save(RequestDTO requestDTO) {
-        Entity entity = getMapper.requestDTOToEntity(requestDTO);
-        getRepository.save(entity);
-        return getMapper.entityToDTO(entity);
-    }
+    DTO update(UUID uuid, RequestDTO requestDTO);
 
-    public List<DTO> getAll() {
-        return getMapper.entityListToDTOList(getRepository.findAll());
-    }
+    DTO getByUUID(UUID uuid);
 
-    @Transactional
-    public DTO update(UUID uuid, RequestDTO requestDTO) {
-        Entity entity = getRepository.findByUuid(uuid).orElse(null);
-        if (entity != null) {
-            entity = getMapper.requestDtoToExistEntity(entity, requestDTO);
-            getRepository.save(entity);
-            return getMapper.entityToDTO(entity);
-        } else {
-            return null;
-        }
-    }
+    Boolean deleteByUUID(UUID uuid);
 
-    public DTO getByUUID(UUID uuid) {
-        Entity entity = getRepository.findByUuid(uuid).orElse(null);
-        if (entity != null) {
-            return getMapper.entityToDTO(entity);
-        } else {
-            return null;
-        }
-    }
-
-    @Transactional
-    public Boolean deleteByUUID(UUID uuid) {
-        Entity entity = getRepository.findByUuid(uuid).orElse(null);
-        if (entity != null) {
-            getRepository.delete(entity);
-            return Boolean.TRUE;
-        } else {
-            return Boolean.FALSE;
-        }
-    }
 }
