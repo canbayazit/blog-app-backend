846cfc92f6fc372c5918c17e9cd8e8baf148d015
Can Bayazit <rovahelsing@gmail.com>
2025-02-24 02:19:17 +0300

    added category entity to generic structure for category

--- src/main/java/com/example/blog_backend/controller/CategoryController.java
+++ src/main/java/com/example/blog_backend/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.example.blog_backend.controller;
 
 import com.example.blog_backend.core.controller.impl.AbstractBaseCrudControllerImpl;
+import com.example.blog_backend.entity.CategoryEntity;
 import com.example.blog_backend.model.requestDTO.CategoryRequestDTO;
 import com.example.blog_backend.model.responseDTO.CategoryResponseDTO;
 import com.example.blog_backend.service.CategoryService;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/category")
 public class CategoryController extends AbstractBaseCrudControllerImpl<
+        CategoryEntity,
         CategoryResponseDTO,
         CategoryRequestDTO,
         CategoryService> {
--- src/main/java/com/example/blog_backend/service/CategoryService.java
+++ src/main/java/com/example/blog_backend/service/CategoryService.java
@@ -1,12 +1,13 @@
 package com.example.blog_backend.service;
 
 import com.example.blog_backend.core.service.BaseCrudService;
+import com.example.blog_backend.entity.CategoryEntity;
 import com.example.blog_backend.model.requestDTO.CategoryRequestDTO;
 import com.example.blog_backend.model.responseDTO.CategoryResponseDTO;
 
 import java.util.Set;
 import java.util.UUID;
 
-public interface CategoryService extends BaseCrudService<CategoryResponseDTO, CategoryRequestDTO> {
+public interface CategoryService extends BaseCrudService<CategoryEntity, CategoryResponseDTO, CategoryRequestDTO> {
     long countCategoryByUuids(Set<UUID> uuids);
 }
