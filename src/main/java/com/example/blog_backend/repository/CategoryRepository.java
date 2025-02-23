16c3aa917dc70e3a3bcfc3bb986c3e366150a5a4
Can Bayazit <rovahelsing@gmail.com>
2025-02-24 02:30:23 +0300

    deleted unnecessary imports

--- src/main/java/com/example/blog_backend/repository/CategoryRepository.java
+++ src/main/java/com/example/blog_backend/repository/CategoryRepository.java
@@ -2,11 +2,9 @@ package com.example.blog_backend.repository;
 
 import com.example.blog_backend.core.repository.BaseRepository;
 import com.example.blog_backend.entity.CategoryEntity;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.UUID;
 
 @Repository
