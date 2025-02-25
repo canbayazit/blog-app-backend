bd3bb1e97e06121e09df0bb90a10169042ff006b
Can Bayazit <rovahelsing@gmail.com>
2025-02-26 01:19:07 +0300

    updated comment request dto with base request dto

--- src/main/java/com/example/blog_backend/model/requestDTO/CommentRequestDTO.java
+++ src/main/java/com/example/blog_backend/model/requestDTO/CommentRequestDTO.java
@@ -1,6 +1,6 @@
 package com.example.blog_backend.model.requestDTO;
 
-import com.example.blog_backend.core.dto.BaseDTO;
+import com.example.blog_backend.core.dto.BaseRequestDTO;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -9,7 +9,7 @@ import lombok.Data;
 import java.util.UUID;
 
 @Data
-public class CommentRequestDTO {
+public class CommentRequestDTO extends BaseRequestDTO {
     @NotBlank(message = "comment is mandatory")
     @Size(max = 500, message = "Comment cannot exceed 500 characters.")
     private String comment;
