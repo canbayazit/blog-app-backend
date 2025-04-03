50b293b872fecf1aec144f259da2b1cd543beaf8
Can Bayazit <rovahelsing@gmail.com>
2025-04-03 04:10:55 +0300

    refactor avoiding dependency injection cycle between role and user mapper

--- src/main/java/com/example/blog_backend/mapper/RoleMapper.java
+++ src/main/java/com/example/blog_backend/mapper/RoleMapper.java
@@ -2,10 +2,71 @@ package com.example.blog_backend.mapper;
 
 import com.example.blog_backend.core.mapper.IBaseMapper;
 import com.example.blog_backend.entity.RoleEntity;
+import com.example.blog_backend.entity.UserEntity;
 import com.example.blog_backend.model.requestDTO.RoleRequestDTO;
-import com.example.blog_backend.model.responseDTO.RoleResponseDTO;
-import org.mapstruct.Mapper;
+import com.example.blog_backend.model.responseDTO.RoleDTO;
+import com.example.blog_backend.model.responseDTO.UserDTO;
+import lombok.RequiredArgsConstructor;
+import org.mapstruct.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 
-@Mapper(componentModel = "spring")
-public interface RoleMapper extends IBaseMapper<RoleResponseDTO, RoleEntity, RoleRequestDTO> {
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.SETTER)
+public abstract class RoleMapper implements IBaseMapper<RoleDTO, RoleEntity, RoleRequestDTO> {
+    protected  UserMapper userMapper;
+
+    public RoleMapper(@Lazy UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
+
+    @Autowired
+    @Lazy
+    public void setUserMapper(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
+
+    @Named("mapUserEntitySetToDtoSet")
+    public Set<UserDTO> mapUserEntitySetToDtoSet(Set<UserEntity> users) {
+        if (users == null) {
+            return null;
+        }
+        return users.stream()
+                .map(userMapper::toDtoWithoutChildren)
+                .collect(Collectors.toSet());
+    }
+
+    @Named("mapUserDtoSetToEntitySet")
+    public Set<UserEntity> mapUserDtoSetToEntitySet(Set<UserDTO> dtos) {
+        if (dtos == null) {
+            return null;
+        }
+        return dtos.stream()
+                .map(userMapper::dtoToEntity)
+                .collect(Collectors.toSet());
+    }
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "uuid", ignore = true)
+    @Mapping(target = "users", ignore = true)
+    public abstract RoleEntity requestDTOToEntity(RoleRequestDTO dto);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "uuid", ignore = true)
+    @Mapping(target = "users", ignore = true)
+    public abstract RoleEntity requestDtoToExistEntity(@MappingTarget RoleEntity entity, RoleRequestDTO requestDTO);
+
+    @Mapping(source = "users", target = "users", qualifiedByName = "mapUserEntitySetToDtoSet")
+    public abstract RoleDTO entityToDTO(RoleEntity entity);
+
+    @Mapping(source = "users", target = "users", qualifiedByName = "mapUserDtoSetToEntitySet")
+    public abstract RoleEntity dtoToEntity(RoleDTO dto);
+
+    @Named("toDtoWithoutUser")
+    @Mapping(target = "users", ignore = true)
+    public abstract RoleDTO toDtoWithoutUser(RoleEntity entity);
+
 }
--- src/main/java/com/example/blog_backend/mapper/UserMapper.java
+++ src/main/java/com/example/blog_backend/mapper/UserMapper.java
@@ -2,12 +2,24 @@ package com.example.blog_backend.mapper;
 
 import com.example.blog_backend.core.mapper.IBaseMapper;
 import com.example.blog_backend.entity.UserEntity;
-import com.example.blog_backend.model.requestDTO.RegisterRequestDTO;
-import com.example.blog_backend.model.requestDTO.UserProfileRequestDTO;
-import com.example.blog_backend.model.responseDTO.UserResponseDTO;
-import org.mapstruct.Mapper;
+import com.example.blog_backend.model.requestDTO.*;
+import com.example.blog_backend.model.responseDTO.UserDTO;
+import com.example.blog_backend.model.responseDTO.UserSummaryDTO;
+import org.mapstruct.*;
 
-@Mapper(componentModel = "spring")
-public interface UserMapper extends IBaseMapper<UserResponseDTO, UserEntity, UserProfileRequestDTO> {
+@Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+        uses = {RoleMapper.class})
+public interface UserMapper extends IBaseMapper<UserDTO, UserEntity, UserRequestDTO> {
+
     UserEntity requestDTOToEntity(RegisterRequestDTO dto);
+    UserEntity requestDtoToExistEntity(@MappingTarget UserEntity entity, UserEmailUpdateRequestDTO dto);
+    UserEntity requestDtoToExistEntity(@MappingTarget UserEntity entity, String dto);
+    UserSummaryDTO entityToUserSummaryDTO(UserEntity entity);
+
+    @Named("toDtoWithoutChildren")
+    @Mapping(target = "roles", ignore = true)
+    UserDTO toDtoWithoutChildren(UserEntity entity);
+
+    @Mapping(source = "roles", target = "roles", qualifiedByName = "toDtoWithoutUser")
+    UserDTO entityToDTO(UserEntity entity);
 }
