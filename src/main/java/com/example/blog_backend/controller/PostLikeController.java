f0357efb3ff815a4853d02ebd15aebfe32ea798b
Can Bayazit <rovahelsing@gmail.com>
2025-04-04 20:14:53 +0300

    refactor post like dto

--- src/main/java/com/example/blog_backend/controller/PostLikeController.java
+++ src/main/java/com/example/blog_backend/controller/PostLikeController.java
@@ -3,7 +3,7 @@ package com.example.blog_backend.controller;
 import com.example.blog_backend.core.controller.impl.AbstractBaseCrudControllerImpl;
 import com.example.blog_backend.entity.PostLikeEntity;
 import com.example.blog_backend.model.requestDTO.PostLikeRequestDTO;
-import com.example.blog_backend.model.responseDTO.PostLikeResponseDTO;
+import com.example.blog_backend.model.responseDTO.PostLikeDTO;
 import com.example.blog_backend.service.PostLikeService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/post/reaction")
 public class PostLikeController extends AbstractBaseCrudControllerImpl<
         PostLikeEntity,
-        PostLikeResponseDTO,
+        PostLikeDTO,
         PostLikeRequestDTO,
         PostLikeService> {
     public PostLikeController(PostLikeService likeService) {
