fa1047648111ac04ccfa86a3bc397c30e3f7910e
Can Bayazit <rovahelsing@gmail.com>
2025-04-29 03:13:54 +0300

    update SecurityConfiguration to use custom unauthorized and access denied handlers

--- src/main/java/com/example/blog_backend/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/blog_backend/configuration/SecurityConfiguration.java
@@ -1,7 +1,10 @@
 package com.example.blog_backend.configuration;
 
 import com.example.blog_backend.util.security.JWTFilter;
+import com.example.blog_backend.util.security.JwtAccessDeniedHandler;
+import com.example.blog_backend.util.security.JwtAuthenticationEntryPoint;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +13,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -24,13 +28,15 @@ import java.util.List;
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
 
-    @Autowired
-    private JWTFilter filter;
+    private final JWTFilter filter;
+    private final JwtAuthenticationEntryPoint unauthorizedHandler;
+    private final JwtAccessDeniedHandler accessDeniedHandler;
 
     private static final String[] AUTH_WHITELIST = {
-            "/auth/**",
+            "/api/auth/**",
             "/swagger-ui/**",
             "v3/api-docs/**",
             "/configuration/security",
@@ -40,27 +46,64 @@ public class SecurityConfiguration {
             "/api/public/**",
             "/api/public/authenticate",
             "/actuator/*",
-            "/swagger-ui/**"
+            "/swagger-ui/**",
+
+            "/api/post/get-all-page-by-filter",
+            "/api/post/comment/get-all-page-by-filter",
+            "api/post/reaction/get-all-page-by-filter",
+
+
+            "/api/post/get/*",
+            "/api/post/get-all",
+
+            "/api/user/get/profile/*",
+            "/api/category/**",
+
+            "/api/context-type/**",
 
     };
 
     private static final String[] USER_AUTH_WHITELIST = {
-            "/selam"
+            "/api/user/update/*",
+
+            "/api/post/create",
+            "/api/post/update/*",
+            "/api/post/publish-request/*",
+            "/api/post/delete/*",
+            "/api/post/my-posts",
+
+            "/api/post/reaction/create",
+            "/api/post/reaction/delete/*",
+
+            "/api/comment/reaction/create",
+            "/api/comment/reaction/delete/*",
+
+            "/api/post/comment/create",
+            "/api/post/comment/reply/*",
+            "/api/post/comment/update/*",
+            "/api/post/comment/delete/*",
+
+            /*"/api/post/comment/get-all",*/
+            "/api/post/comment/get/*", // geçici adminde olacak userda değil test amaçlı koydum
+
+  /*          "/api/post/get-all-page-by-filter",*/
     };
 
     private static final String[] USER_ADMIN_AUTH_WHITELIST = {
-            "/user/**",
+            /*"/user/delete/**",*/
     };
 
     private static final String[] ADMIN_AUTH_WHITELIST = {
-            "/role/**",
-            "/admin/**",
+            "/api/role/**",
+            "/api/admin/**",
+            /*"/api/comment/get-all",
+            "/api/comment/get/*"*/
     };
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // CSRF koruması devre dışı
+                .csrf(AbstractHttpConfigurer::disable) // CSRF koruması devre dışı
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration configuration = new CorsConfiguration();
                     configuration.setAllowedOrigins(List.of("*"));
@@ -73,17 +116,17 @@ public class SecurityConfiguration {
                     auth
                             .requestMatchers(AUTH_WHITELIST).permitAll()
                             .requestMatchers(USER_ADMIN_AUTH_WHITELIST).hasAnyRole("ADMIN","USER")
-                            .requestMatchers(ADMIN_AUTH_WHITELIST).hasRole("ADMIN")
-                            .requestMatchers(USER_AUTH_WHITELIST).hasRole("USER");
+                            .requestMatchers(USER_AUTH_WHITELIST).hasRole("USER")
+                            .requestMatchers(ADMIN_AUTH_WHITELIST).hasRole("ADMIN");
+
                             /*.anyRequest().authenticated();*/ // Diğer tüm istekler doğrulama gerektirir
                 })
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Stateless session yönetimi
-                .exceptionHandling(exceptions -> exceptions
-                        .authenticationEntryPoint(
-                                (request, response, authException) ->
-                                        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized")
-                        ))
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(unauthorizedHandler)
+                        .accessDeniedHandler(accessDeniedHandler)
+                )
                 .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class); // JWT Filtresi
 
         return http.build();
